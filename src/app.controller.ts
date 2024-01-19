4a6637c8221a38123da4a5500ef746d933cde830
codeForMobile <code.mobile@protonmail.com>
2024-01-18 20:42:18 -0600

    working in tandem with gateway & auth

---
+++ src/app.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Inject, OnModuleInit } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ClientKafka, EventPattern } from '@nestjs/microservices';
+
+@Controller()
+export class AppController implements OnModuleInit{
+  constructor(
+    private readonly appService: AppService, 
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientKafka
+    ) {}
+
+  onModuleInit() {
+    this.authClient.subscribeToResponseOf('get_user')
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @EventPattern('order_created')
+  handleOrderCreated(data:any) {
+    this.appService.handleOrderCreated(data)
+  }
+}
